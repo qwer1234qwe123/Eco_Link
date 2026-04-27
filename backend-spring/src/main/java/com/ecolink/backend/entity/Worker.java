2c9d03ba86531f4543c79f6ddf545a986689184c
Chan-pj <gjrjs730@gmail.com>
2026-04-27 15:04:14 +0900

    프론트엔드 파일 추가 연동

--- backend-spring/src/main/java/com/ecolink/backend/entity/Worker.java
+++ backend-spring/src/main/java/com/ecolink/backend/entity/Worker.java
@@ -1,9 +1,40 @@
+// package com.ecolink.backend.entity;
+
+// import jakarta.persistence.*;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import java.util.List;
+
+// @Entity
+// @Table(name = "worker")
+// @Getter
+// @NoArgsConstructor
+// public class Worker {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     @Column(name = "username", nullable = false, length = 50, unique = true)
+//     private String username;
+
+//     @Column(name = "password", nullable = false, length = 255)
+//     private String password;
+
+//     @Column(name = "vehicle_number", length = 20)
+//     private String vehicleNumber;
+
+//     @OneToMany(mappedBy = "worker")
+//     private List<CollectionRoute> collectionRoutes;
+// }
+
 package com.ecolink.backend.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "worker")
@@ -24,6 +55,7 @@ public class Worker {
     @Column(name = "vehicle_number", length = 20)
     private String vehicleNumber;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "worker")
     private List<CollectionRoute> collectionRoutes;
 }
