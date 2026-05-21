696c47595178a33fe8624537e3179cb211f0e0d1
nakwol <rkdeodnd100@naver.com>
2026-05-21 11:55:55 +0900

    주석 update

--- backend-spring/src/main/java/com/ecolink/backend/controller/TrashcanController.java
+++ backend-spring/src/main/java/com/ecolink/backend/controller/TrashcanController.java
@@ -29,19 +29,22 @@ public class TrashCanController {
 
 
     // 추가
-    // POST /api/trashcan > 새 쓰레기통 추가
+    // [ 생성 ] POST /api/trashcan > 새 쓰레기통 추가
+    // 프론트에서 보낸 쓰레기통 정보 (이름, 위도, 경도, 용량)등을 받아서 DB에 저장
     @PostMapping
 public ResponseEntity<TrashCan> create(@RequestBody TrashCanRequest request) {
     return ResponseEntity.ok(trashCanService.create(request));
 }
 
-// PUT /api/trashcan/{id} > id에 해당하는 쓰레기통 수정
+// [ 수정 ] PUT /api/trashcan/{id} > id에 해당하는 쓰레기통 수정
+// URL의 id로 쓰레기통을 찾아서 프론트에서 보낸 새 정보로 DB 업데이트
 @PutMapping("/{id}")
 public ResponseEntity<TrashCan> update(@PathVariable Long id, @RequestBody TrashCanRequest request) {
     return ResponseEntity.ok(trashCanService.update(id, request));
 }
 
-// DELECT /api/trashcan/{id} > id 에 해당하는 쓰레기통 삭제
+// [ 삭제 ] DELECT /api/trashcan/{id} > id 에 해당하는 쓰레기통 삭제
+// URL의 id로 쓰레기통을 찾아서 DB에서 삭제, 완료되면 빈 응답(204) 반환한다.
 @DeleteMapping("/{id}")
 public ResponseEntity<Void> delete(@PathVariable Long id) {
     trashCanService.delete(id);
